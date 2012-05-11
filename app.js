72272244a69989b8b5f6889fc07aa78297c99205
Chihiro SAKATOKU <csakatoku@gmail.com>
2012-05-12 01:49:04 +0900

    Use plates to server HTML contents.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var flatiron = require('flatiron'),
+    plates = require('plates'),
     path = require('path'),
     app = flatiron.app;
 
@@ -7,7 +8,11 @@ app.config.file({ file: path.join(__dirname, 'config', 'config.json') });
 app.use(flatiron.plugins.http);
 
 app.router.get('/', function () {
-  this.res.json({ 'hello': 'flatiron' });
+    var html = '<html><h1 id="greet">Hello World</h1></html>';
+    var data = { "greet": "こんにちは世界" };
+    var output = plates.bind(html, data);
+    this.res.writeHead(200, { 'Content-Type': 'text/html;charset=UTF-8' });
+    this.res.end(output);
 });
 
 app.start(3000);
