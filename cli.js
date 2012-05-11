de446b9e0bb59168af1feb4b47e00b39a6c63a2e
Chihiro SAKATOKU <csakatoku@gmail.com>
2012-05-12 01:20:44 +0900

    Added a CLI app

---
+++ cli.js
@@ -0,0 +1,16 @@
+var flatiron = require('flatiron'),
+    path = require('path'),
+    app = flatiron.app;
+
+app.config.file({ file: path.join(__dirname, 'config', 'config.json') });
+
+app.use(flatiron.plugins.cli, {
+    source: path.join(__dirname, 'lib', 'commands'),
+    usage: 'Empty Flatiron Application, please fill out commands'
+});
+
+app.cmd('version', function() {
+    console.log('flatiron ' + flatiron.version);
+});
+
+app.start();
