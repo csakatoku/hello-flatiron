d1f550da179c33bc0e6c5eff5f24a199ac89e067
Chihiro SAKATOKU <csakatoku@gmail.com>
2012-05-12 19:21:31 +0900

    Added a custom plugin

--- app.js
+++ app.js
@@ -7,11 +7,16 @@ app.config.file({ file: path.join(__dirname, 'config', 'config.json') });
 
 app.use(flatiron.plugins.http);
 
+// Custom plugin to say just hello world
+var helloworld = require('./lib/plugins/helloworld.js');
+app.use(helloworld);
+
 app.router.get('/', function () {
     var html = '<html><h1 id="greet">Hello World</h1></html>';
     var data = { "greet": "こんにちは世界" };
     var output = plates.bind(html, data);
 
+    app.hello("Flatiron");
     app.log.debug("request headers", this.req.headers);
 
     this.res.writeHead(200, { 'Content-Type': 'text/html;charset=UTF-8' });
