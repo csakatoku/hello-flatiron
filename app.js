ca0a0180f91e6dde24599fc312845bb2ade04627
Chihiro SAKATOKU <csakatoku@gmail.com>
2012-05-12 19:03:54 +0900

    Added a sample of routing by HTTP method. Test logging usage.

--- app.js
+++ app.js
@@ -11,8 +11,26 @@ app.router.get('/', function () {
     var html = '<html><h1 id="greet">Hello World</h1></html>';
     var data = { "greet": "こんにちは世界" };
     var output = plates.bind(html, data);
+
+    app.log.debug("request headers", this.req.headers);
+
     this.res.writeHead(200, { 'Content-Type': 'text/html;charset=UTF-8' });
     this.res.end(output);
+});
+
+app.router.get('/test/:name', function(name) {
+    this.res.writeHead(200, { 'Content-Type': 'text/html;charset=UTF-8' });
+    this.res.end('/test/' + name + '?_method=get');
+});
+
+app.router.post('/test/:name', function(name) {
+    this.res.writeHead(200, { 'Content-Type': 'text/html;charset=UTF-8' });
+    this.res.end('/test/' + name + '?_method=post');
+});
+
+app.router.get('/test', function() {
+    this.res.writeHead(200, { 'Content-Type': 'text/html;charset=UTF-8' });
+    this.res.end('/test');
 });
 
 app.start(3000);
